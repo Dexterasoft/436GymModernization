73046c67d8df9dc426048e09a16457380f6664d2
albabe1 <albabe1@umbc.edu>
2018-05-14 20:19:02 -0400

    Php file displays information entered.
    Added name="message" to textarea.
    Adjusted Select width.

---
+++ Deliverable5/ContactInfo.php
@@ -0,0 +1,24 @@
+<?php
+	
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$msgtitle=$_POST['msgtitle'];
+	$purpose=$_POST['purpose'];
+	$message=$_POST['message`'];
+?>
+
+<Center>
+
+Name: <?=$name?>
+<Br>
+Email: <?=$email?>
+<Br>
+Message Title: <?=$msgtitle?>
+<Br>
+Purpose: <?=$purpose?>
+<Br>
+Message: <Br>
+&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp <?=$purpose?>
+<Br>
+
+</Center>
